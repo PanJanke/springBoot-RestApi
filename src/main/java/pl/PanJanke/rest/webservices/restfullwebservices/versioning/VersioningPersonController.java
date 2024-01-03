ef578d2daa05c3ad5a73d5512292682cb5b6e830
Jan Krol <jan.krol.legowski@gmail.com>
2024-01-03 21:36:21 +0100

    other URI versioning

--- src/main/java/pl/PanJanke/rest/webservices/restfullwebservices/versioning/VersioningPersonController.java
+++ src/main/java/pl/PanJanke/rest/webservices/restfullwebservices/versioning/VersioningPersonController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+
+    //URI versioning
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob Charlie");
@@ -12,6 +14,42 @@ public class VersioningPersonController {
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", " Charlie"));
+
+    }
+
+    //Request Parameter versioning
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", " Charlie"));
+
+    }
+
+    //headers versioning
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", " Charlie"));
+
+    }
+
+    //media type versioning
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
         return new PersonV2(new Name("Bob", " Charlie"));
 
     }
